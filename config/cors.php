ed22c02e67c9b2bb9a2cf33799a123d57aad240f
Shoki Ueno <shokiueno.ac@gmail.com>
2025-06-06 18:04:29 +0900

    CRUD実装pt2

--- config/cors.php
+++ config/cors.php
@@ -1,15 +1,20 @@
 <?php
 
+return [
+
+    //どのパスでCORSを許可するか
 'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
+    //どのHTTPメソッドを許可するか
 'allowed_methods' => ['*'],
-
+    //どのオリジン(domain:port)からのリクエストを許可するか
 'allowed_origins' => ['http://localhost:5173'],  // React:Vite の URL
-
+    //オリジン中でどのヘッダーを許可するか
 'allowed_headers' => ['*'],
-
+    //ブラウザにさらす(Exposeする)ヘッダー
 'exposed_headers' => [],
-
+    //プリフライトリクエストのキャッシュ有効期限
 'max_age' => 0,
-
+    //Cookie(認証情報)を含むリクエストを受け付けるか
 'supports_credentials' => true,
+
+];
